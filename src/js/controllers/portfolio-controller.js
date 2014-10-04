d6a4ec27cb5cc48419b88ffc7ddbf87d78854411
nickswhite89@gmail.com <nickswhite89@gmail.com>
2014-10-04 12:34:00 +0100

    Use ng-switch

--- src/js/controllers/portfolio-controller.js
+++ src/js/controllers/portfolio-controller.js
@@ -2,21 +2,23 @@ angular
     .module('app.controllers')
     .controller('portfolio', function ($scope, $sce, projectResource, lodash) {
         $scope.portfolio = true;
-        $scope.project = false;
-
         $scope.projects = {};
+
         projectResource.query(function (response) {
             $scope.projects = response;
         });
 
         $scope.showPortfolio = function (pid) {
             $scope.portfolio = false;
-            $scope.project = true;
 
             $scope.showProject = lodash.findWhere($scope.projects, {
                 id: pid
             });
 
             $scope.projectText = $sce.trustAsHtml($scope.showProject.text);
+        };
+
+        $scope.close = function () {
+            $scope.portfolio = true;
         };
     });
